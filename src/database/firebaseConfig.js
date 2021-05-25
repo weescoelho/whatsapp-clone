e477526a97e198736cf2b62aa84c4a33c59ad89b
weescoelho <wes.coelho18@gmail.com>
2021-05-25 19:06:53 -0300

    chore(firebase): adicionado arquivo de configuração do firebase

---
+++ src/database/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDcWmUJ51e4N-V8HS03tSaaG3o5NMXTWgo",
+  authDomain: "whatsappclone-ad3ee.firebaseapp.com",
+  projectId: "whatsappclone-ad3ee",
+  storageBucket: "whatsappclone-ad3ee.appspot.com",
+  messagingSenderId: "161554700965",
+  appId: "1:161554700965:web:013a41fccab182b366cb4c"
+};
